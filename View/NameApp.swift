c5ad7496ff6ada393867c2169485132379109f96
Jennifer <jehii95@live.com>
2019-08-26 12:35:32 -0500

    parte grafica inicio Vc One sin conexiones ni constraints

---
+++ View/NameApp.swift
@@ -0,0 +1,18 @@
+//
+//  NameApp.swift
+//  Netix
+//
+//  Created by Jenifer on 8/26/19.
+//  Copyright © 2019 Jenifer. All rights reserved.
+//
+
+import UIKit
+
+class NameApp: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ View/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Netix
+//
+//  Created by Jenifer on 8/23/19.
+//  Copyright © 2019 Jenifer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
